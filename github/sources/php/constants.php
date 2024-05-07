ae79ad66d5058df0d5b66ce10bfa5c620479d1c7
Maelinou61 <mael.rimeur@gmail.com>
2024-05-07 18:32:08 +0200

    Second version

---
+++ github/sources/php/constants.php
@@ -0,0 +1,8 @@
+<?php
+//Database constants.
+define('DB_USER', 'user');
+define('DB_PASSWORD', 'password');
+define('DB_NAME', 'chronic_pain');
+define('DB_SERVER', 'db');
+define('DB_PORT', '3306');
+?>
